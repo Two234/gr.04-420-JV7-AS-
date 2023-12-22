6cc5ff75bf5d10c04cb68c173d312175eed1c818
Two234 <bernylambert28@gmail.com>
2023-12-22 15:59:22 -0500

    enemy Spawner test

--- .idea/.idea.gr.04-420-JV7-AS-/.idea/sonarlint/issuestore/index.pb
+++ .idea/.idea.gr.04-420-JV7-AS-/.idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,11 @@ O
 Assets/Script/PlayerMovement.cs,d\2\d22aaaa89ae3f8f0d4ba903b6a0d8d8cefdaf19a
 {
 KLibrary/PackageCache/com.unity.2d.tilemap@1.0.0/Editor/GridPaintingState.cs,a\f\af197e96cdbadb3908dbc9acd75499698d1d10a3
+N
+Assets/Script/MonsterDamage.cs,c\7\c72849de61d741a4ab902c56c1d8396f4b1664a1
+M
+Assets/Script/EnemySpawner.cs,0\1\018c73fbb917fc8dd9cb71777a44a096ecd135ad
+M
+Assets/Script/PlayerHealth.cs,d\1\d158e0e5c47027cfef55d18073a88d0e3ac0310f
+R
+"Assets/Script/DestroyController.cs,1\7\17e9b0427fc30dfd4fdc3650c46f030776693f0a
--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private float spawnRate = 1f;
+    [SerializeField] private GameObject[] enemyPrefabs ;
+    [SerializeField] private bool canSpawn =true; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Spawner());
     }
+
+    private IEnumerator Spawner()
+    {
+        WaitForSeconds wait = new WaitForSeconds(spawnRate);
+        while (canSpawn)
+        {
+            yield return wait;
+            int rand = Random.Range(0, enemyPrefabs.Length);
+            GameObject enemyToSpawn = enemyPrefabs[rand];
+
+            Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+        }
+    }
+
+
+
 
     // Update is called once per frame
     void Update()
