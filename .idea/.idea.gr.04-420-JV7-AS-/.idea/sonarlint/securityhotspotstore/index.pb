af70f38231f85481c386a27baccffeec957b816c
Two234 <bernylambert28@gmail.com>
2023-12-21 22:24:56 -0500

    Finssed player code
    Added hammer and finessed the plyaer code and added destructable wall

--- .idea/.idea.gr.04-420-JV7-AS-/.idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/.idea.gr.04-420-JV7-AS-/.idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,5 @@
 
 O
 Assets/Script/PlayerMovement.cs,d\2\d22aaaa89ae3f8f0d4ba903b6a0d8d8cefdaf19a
+{
+KLibrary/PackageCache/com.unity.2d.tilemap@1.0.0/Editor/GridPaintingState.cs,a\f\af197e96cdbadb3908dbc9acd75499698d1d10a3
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rbHorizontal; 
-    private Rigidbody2D rbVertical;   
+    private Rigidbody2D rbHorizontal;
+    private Rigidbody2D rbVertical;
     private BoxCollider2D coll;
 
     private float dirX;
-    private float dirY; 
+    private float dirY;
+    
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 8f;
-    [SerializeField] private float verticalMoveSpeed = 5f; 
-    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float verticalMoveSpeed = 5f;
+    [SerializeField] private int nbrMovementTotal = 12;
+    private int nbrMovmentLeft;
+
     private SpriteRenderer _sprite;
 
     [SerializeField] private AudioSource jumpSound;
@@ -23,24 +26,52 @@ public class PlayerMovement : MonoBehaviour
     {
         coll = GetComponent<BoxCollider2D>();
         rbHorizontal = GetComponent<Rigidbody2D>();
-        rbVertical =GetComponent<Rigidbody2D>(); 
+        rbVertical = GetComponent<Rigidbody2D>();
         _sprite = GetComponent<SpriteRenderer>();
+        nbrMovmentLeft = nbrMovementTotal;
     }
 
-   
+
     private void Update()
     {
-        dirX = Input.GetAxisRaw("Horizontal");
-        dirY = Input.GetAxisRaw("Vertical");
-
-       
-        rbHorizontal.velocity = new Vector2(dirX * moveSpeed, rbHorizontal.velocity.y);
-
         
-        rbVertical.velocity = new Vector2(rbVertical.velocity.x, dirY * verticalMoveSpeed);
+        if (nbrMovmentLeft > 0)
+        {
+            
+            dirX = Input.GetAxisRaw("Horizontal");
+            dirY = Input.GetAxisRaw("Vertical");
 
-        UpdateAnimationState();
+           
+            bool hasInput = (dirX != 0f || dirY != 0f);
+
+            
+            if (hasInput && (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)))
+            {
+                nbrMovmentLeft--;
+                rbHorizontal.velocity = new Vector2(dirX * moveSpeed, rbHorizontal.velocity.y);
+                rbVertical.velocity = new Vector2(rbVertical.velocity.x, dirY * verticalMoveSpeed);
+
+                
+                Debug.Log("Movements Left: " + nbrMovmentLeft);
+            }
+
+            
+            if (hasInput)
+            {
+                UpdateAnimationState();
+            }
+        }
+        else
+        {
+            
+            rbHorizontal.velocity = Vector2.zero;
+            rbVertical.velocity = Vector2.zero;
+
+            
+            Debug.Log("No movements left.");
+        }
     }
+
 
     private void UpdateAnimationState()
     {
@@ -52,10 +83,5 @@ public class PlayerMovement : MonoBehaviour
         {
             _sprite.flipX = true;
         }
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, groundLayer);
     }
 }
